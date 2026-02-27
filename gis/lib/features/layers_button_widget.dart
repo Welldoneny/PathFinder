1b30794edc679ee5ea29362da66d7e4f4a84c82e
unknown <VladislavIzov@gmail.com>
2026-02-27 15:17:32 +0700

    geolocator

--- gis/lib/features/layers_button_widget.dart
+++ gis/lib/features/layers_button_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:path_finder/styles.dart/button_style.dart';
 import 'package:path_finder/widgets/map_tile_widgeet.dart';
-
 
 class MapTypeButton extends StatelessWidget {
   final ValueChanged<String> onSelected;
@@ -9,70 +9,67 @@ class MapTypeButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      top: 30,
-      right: 16,
-      child: PopupMenuButton<String>(
-        padding: const EdgeInsets.all(8),
-        constraints: const BoxConstraints(minWidth: 140, maxWidth: 280),
-        onSelected: onSelected,
-        itemBuilder: (context) => [
-          PopupMenuItem(
-            padding: EdgeInsets.zero,
-            enabled: false,
-            child: SizedBox(
-              width: 280,
-              child: GridView.count(
-                crossAxisCount: 2,
-                shrinkWrap: true,
-                crossAxisSpacing: 8,
-                mainAxisSpacing: 8,
-                physics: const NeverScrollableScrollPhysics(),
-                padding: const EdgeInsets.all(18),
-                children: [
-                  buildMapTileWidget(
-                    context,
-                    'OpenStreetMap',
-                    'assets/images/openstreetmap.png',
-                    'osm',
-                  ),
-                  buildMapTileWidget(
-                    context,
-                    'Landscape',
-                    'assets/images/landscape.png',
-                    'lds',
-                  ),
-                  buildMapTileWidget(
-                    context,
-                    'Satellite',
-                    'assets/images/satelite.png',
-                    'sat',
-                  ),
-                  buildMapTileWidget(
-                    context,
-                    'Atlas',
-                    'assets/images/atlas.png',
-                    'atl',
-                  ),
-                  buildMapTileWidget(
-                    context,
-                    'Outdoors',
-                    'assets/images/outdoors.png',
-                    'out',
-                  ),
-                  buildMapTileWidget(
-                    context,
-                    'OpenCycleMap',
-                    'assets/images/opencyclemap.png',
-                    'ocm',
-                  ),
-                ],
-              ),
+    return PopupMenuButton<String>(
+      padding: const EdgeInsets.all(8),
+      constraints: const BoxConstraints(minWidth: 140, maxWidth: 280),
+      onSelected: onSelected,
+      itemBuilder: (context) => [
+        PopupMenuItem(
+          padding: EdgeInsets.zero,
+          enabled: false,
+          child: SizedBox(
+            width: 280,
+            child: GridView.count(
+              crossAxisCount: 2,
+              shrinkWrap: true,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+              physics: const NeverScrollableScrollPhysics(),
+              padding: const EdgeInsets.all(18),
+              children: [
+                buildMapTileWidget(
+                  context,
+                  'OpenStreetMap',
+                  'assets/images/openstreetmap.png',
+                  'osm',
+                ),
+                buildMapTileWidget(
+                  context,
+                  'Landscape',
+                  'assets/images/landscape.png',
+                  'lds',
+                ),
+                buildMapTileWidget(
+                  context,
+                  'Satellite',
+                  'assets/images/satelite.png',
+                  'sat',
+                ),
+                buildMapTileWidget(
+                  context,
+                  'Atlas',
+                  'assets/images/atlas.png',
+                  'atl',
+                ),
+                buildMapTileWidget(
+                  context,
+                  'Outdoors',
+                  'assets/images/outdoors.png',
+                  'out',
+                ),
+                buildMapTileWidget(
+                  context,
+                  'OpenCycleMap',
+                  'assets/images/opencyclemap.png',
+                  'ocm',
+                ),
+              ],
             ),
           ),
-        ],
-        child: const Icon(Icons.layers),
-      ),
+        ),
+      ],
+      style: buttonStyle,
+      icon: const Icon(Icons.layers, color: Colors.black,),
     );
   }
 }
